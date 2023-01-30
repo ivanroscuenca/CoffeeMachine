807636ae3989e67be31e5e0f3efcf5367a23bc0b
Your Name <ivanros@protonmail.com>
2023-01-30 12:17:42 +0100

    Updated comments

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def is_transaction_successful(money_received, drink_cost):
 def make_coffee(drink_name, order_ingredients):
     for item in order_ingredients:
         resources[item] -= order_ingredients[item]
-    print(f"Here is your drink : {drink_name} ☕️ enjoy!")
+    print(f"Here is your drink : {drink_name} ☕️ enjoy it!")
 
 
 is_on = True
@@ -95,7 +95,7 @@ while is_on:
 # a. Check the user’s input to decide what to do next.
 # b. The prompt should show every time action has completed, e.g. once the drink is
 # dispensed. The prompt should show again to serve the next customer.
-# TODO: 2. Turn off the Coffee Machine by entering “​off”​to the prompt.
+# TODO: 2. Turn off the Coffee Machine by entering “off”to the prompt.
 # a. For maintainers of the coffee machine, they can use “off” as the secret word to turn off
 # the machine. Your code should end execution when this happens.
 # TODO: 3. Print report.
@@ -108,7 +108,7 @@ while is_on:
 # a. When the user chooses a drink, the program should check if there are enough resources to make that drink.
 # b. E.g. if Latte requires 200ml water but there is only 100ml left in the machine. It should not continue
 # to make the drink but print:
-# “​Sorry there is not enough water.”​
+# “Sorry there is not enough water.”
 # c. The same should happen if another resource is depleted, e.g. milk or coffee.
 # TODO: 5. Process coins.
 # a. If there are sufficient resources to make the drink selected, then the program should
@@ -118,8 +118,8 @@ while is_on:
 # pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
 # TODO: 6. Check transaction successful?
 # a. Check that the user has inserted enough money to purchase the drink they selected. E.g Latte cost $2.50,
-# but they only inserted $0.52 then after counting the coins the program should say “​Sorry that's not enough money.
-# Money refunded.​”.
+# but they only inserted $0.52 then after counting the coins the program should say “Sorry that's not enough money.
+# Money refunded.”.
 # b. But if the user has inserted enough money, then the cost of the drink gets added to the machine as the profit and
 # this will be reflected the next time “report” is triggered. E.g. Water: 100ml
 # Milk: 50ml
